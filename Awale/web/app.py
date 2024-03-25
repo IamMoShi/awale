3b9dd250b170e182432d641a1c33db89bcf86fdd
Leo F <leofornoff02@gmail.com>
2024-03-25 19:34:57 +0100

    add JS to synchronize the display of the game without recharging the page

--- Awale/web/app.py
+++ Awale/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,3 +6,25 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+
+@app.route("/current_board")
+def curren_board():
+    dict = {
+        "board": [5] * 12,
+        "playable": [1] * 6 + [0] * 6,
+        "score1": 0,
+        "score2": 0,
+    }
+    return jsonify(dict)
+
+
+@app.route("/play/<int:playable>")
+def play(playable):
+    dict = {
+        "board": [playable] * 12,
+        "playable": [0] * 6 + [1] * 6,
+        "score1": playable + 1,
+        "score2": playable ,
+    }
+    return jsonify(dict)
