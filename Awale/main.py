6ebbf931d9ae3f6f88b50a20b1881eec27ab14a5
Michaël Yang <michael.yang@telecomnancy.eu>
2024-04-05 13:07:16 +0200

    jeu awale sans les tests

---
+++ Awale/main.py
@@ -0,0 +1,10 @@
+from Player import Player
+from Board import Board
+from Game import Game
+
+
+if __name__ == "__main__" :
+    name1 = input("Entrez le nom du joueur 1 : ")
+    name2 = input("Entrez le nom du joueur 2 : ")
+    game = Game(name1, name2)
+    game.play_game()
