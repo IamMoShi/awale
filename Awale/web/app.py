6d1f178fef3f0b63ec01cf5172f273ad8daf7264
Elise Neyens <elise.neyens@telecomnancy.eu>
2024-04-06 19:27:46 +0200

    Adaptation front back du jeu

--- Awale/web/app.py
+++ Awale/web/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, jsonify
+from Game import Game
 
 app = Flask(__name__)
 
+game = Game("Player1", "Player2")
+
+@app.route("/reset")
+def reset():
+    game.reset()
+    return "Game reset"
 
 @app.route("/")
 def hello_world():
@@ -10,21 +17,28 @@ def hello_world():
 
 @app.route("/current_board")
 def curren_board():
+    board, playable, score1, score2 = game.get_current_board()
+    playable = [1 if i in playable else 0 for i in range(12)]
     dict = {
-        "board": [5] * 12,
-        "playable": [1] * 6 + [0] * 6,
-        "score1": 0,
-        "score2": 0,
+        "board": board,
+        "playable": playable,
+        "score1": score1,
+        "score2": score2,
     }
     return jsonify(dict)
 
 
 @app.route("/play/<int:playable>")
 def play(playable):
+    print("player wants to play : " + str(playable))
+    game.play(playable)
+    board, playable, score1, score2 = game.get_current_board()
+    print("next playable cases : " + str(playable))
+    playable = [1 if i in playable else 0 for i in range(12)]
     dict = {
-        "board": [playable] * 12,
-        "playable": [0] * 6 + [1] * 6,
-        "score1": playable + 1,
-        "score2": playable ,
+        "board": board,
+        "playable": playable,
+        "score1": score1,
+        "score2": score2,
     }
     return jsonify(dict)
--- Awale/web/main.py
+++ Awale/web/main.py
@@ -1,9 +1,7 @@
-from Player import Player
-from Board import Board
-from Game import Game
+from web.Game import Game
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     name1 = input("Entrez le nom du joueur 1 : ")
     name2 = input("Entrez le nom du joueur 2 : ")
     game = Game(name1, name2)
